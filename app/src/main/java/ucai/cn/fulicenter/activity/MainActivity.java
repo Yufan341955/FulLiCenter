b2346be97267bbf122b6426abce858f28b1b9a91
Yufan341955 <861487325@qq.com>
2016-10-14 18:46:40 +0800

    修改MainAcitivity布局文件bug,实现RadioButton互斥效果

--- app/src/main/java/ucai/cn/fulicenter/activity/MainActivity.java
+++ app/src/main/java/ucai/cn/fulicenter/activity/MainActivity.java
@@ -1,19 +1,66 @@
 package ucai.cn.fulicenter.activity;
 
-import android.content.Intent;
-import android.os.SystemClock;
-import android.provider.Settings;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 import ucai.cn.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
-
+   RadioButton rbtnNewGoods,rbtnBoutique,rbtnCategory,rbtnCart,rbtnProsen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        rbtnNewGoods= (RadioButton) findViewById(R.id.rbtn_newGoods);
+        rbtnBoutique= (RadioButton) findViewById(R.id.rbtn_boutique);
+        rbtnCategory= (RadioButton) findViewById(R.id.rbtn_category);
+        rbtnCart= (RadioButton) findViewById(R.id.rbtn_cart);
+        rbtnProsen= (RadioButton) findViewById(R.id.rbtn_prosen);
+    }
+
+    public void onCheckedChange(View v){
+        switch (v.getId()){
+            case R.id.rbtn_newGoods:
+                singleChecked((RadioButton) v);
+            break;
+            case R.id.rbtn_boutique:
+                singleChecked((RadioButton) v);
+            break;
+            case R.id.rbtn_category:
+                singleChecked((RadioButton) v);
+            break;
+            case R.id.rbtn_cart:
+                singleChecked((RadioButton) v);
+            break;
+            case R.id.rbtn_prosen:
+                singleChecked((RadioButton) v);
+            break;
+        }
+    }
+    public void singleChecked(RadioButton radio){
+        if(radio!=rbtnNewGoods){
+            rbtnNewGoods.setChecked(false);
+        }
+        if(radio!=rbtnBoutique){
+            rbtnBoutique.setChecked(false);
+        }
+        if(radio!=rbtnCategory){
+            rbtnCategory.setChecked(false);
+        }
+        if(radio!=rbtnCart){
+            rbtnCart.setChecked(false);
+        }
+        if(radio!=rbtnProsen){
+            rbtnProsen.setChecked(false);
+        }
+
     }
 
 
