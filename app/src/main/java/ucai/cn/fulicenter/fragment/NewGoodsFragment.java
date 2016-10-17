dc07f9fd2b3e7daee08fe92c1a1dfcae47aafbc8
Yufan341955 <861487325@qq.com>
2016-10-17 15:19:46 +0800

    添加fragment布局

--- app/src/main/java/ucai/cn/fulicenter/fragment/NewGoodsFragment.java
+++ app/src/main/java/ucai/cn/fulicenter/fragment/NewGoodsFragment.java
@@ -24,7 +24,8 @@ public class NewGoodsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_goods, container, false);
+        View view=inflater.inflate(R.layout.fragment_new_goods, container, false);
+        return view;
     }
 
 }
