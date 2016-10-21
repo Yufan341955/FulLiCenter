23affcafffdf0b07da0627c7d3722dbf5f0cea55
Yufan341955 <861487325@qq.com>
2016-10-21 10:26:50 +0800

    添加登录页面，完成登录页面布局

---
+++ app/src/main/java/ucai/cn/fulicenter/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package ucai.cn.fulicenter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ucai.cn.fulicenter.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
